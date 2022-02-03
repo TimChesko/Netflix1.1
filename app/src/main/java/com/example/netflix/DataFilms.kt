4141edfcb1ee38e5f96fc51df6735953e98abe4b
Timofey <timofey.lazarev@gmail.com>
2022-02-04 00:36:15 +0300

    Не рабочий вариант, но понял что import...R все ломает

--- app/src/main/java/com/example/netflix/DataFilms.kt
+++
@@ -1,5 +0,0 @@
-package com.example.netflix
-
-import android.media.Image
-
-data class DataFilms(var id_film: Int, var name: String, var description: String, val rate: Int, var image: Image)
--- app/src/main/java/com/example/netflix/MainActivity.kt
+++ app/src/main/java/com/example/netflix/MainActivity.kt
@@ -2,15 +2,38 @@ package com.example.netflix
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.main_activity)
-
+        setContentView(R.layout.activity_main)
+        initDrawer()
     }
 
+    private fun initDrawer() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.setOnItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId) {
+            R.id.navigation_home -> {
+                showFragment(HomeFragment.newInstance());
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    fun showFragment(fragment: Fragment?) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.commit()
+    }
 }

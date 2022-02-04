870b557f81cf7d3a5027949d12fe1d326934e6d8
Timofey <timofey.lazarev@gmail.com>
2022-02-04 11:16:50 +0300

    исправил OnItemSelectedListener но все еще не работает

--- app/src/main/java/com/example/netflix/MainActivity.kt
+++ app/src/main/java/com/example/netflix/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,23 +17,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initDrawer() {
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
-        bottomNavigationView.setOnItemSelectedListener(mOnNavigationItemSelectedListener)
+        val navigationBarView = findViewById<NavigationBarView>(R.id.nav_view)
+        navigationBarView.setOnItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+    private val mOnNavigationItemSelectedListener = NavigationBarView.OnItemSelectedListener { menuItem ->
         when (menuItem.itemId) {
             R.id.navigation_home -> {
-                showFragment(HomeFragment.newInstance());
-                return@OnNavigationItemSelectedListener true
+                showFragment(HomeFragment.newInstance())
+                return@OnItemSelectedListener true
             }
         }
         false
     }
 
-    fun showFragment(fragment: Fragment?) {
+    private fun showFragment(fragment: Fragment?) {
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.commit()
+        fragmentTransaction.add(R.id.fragment_container, fragment!!).commit()
     }
 }
